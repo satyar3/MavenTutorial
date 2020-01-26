aa3c861f63daaa67e4aacf1fd14b6af4eb872b90
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-01-26 10:47:31 +0530

    Updating dependancy

--- src/test/java/MavenTutorial/MavenTutorial/AppTest.java
+++ src/test/java/MavenTutorial/MavenTutorial/AppTest.java
@@ -1,51 +1,30 @@
 package MavenTutorial.MavenTutorial;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	@Test
+	public void testApp()
+	{
+		//Without profile
+		//-DEnvname=UAT clean install
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+		//With profile
+		//-PSIT -DEnvname=SITENV clean test
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-        
-        //Without profile
-        //-DEnvname=UAT clean install
-        
-        //With profile
-        //-PSIT -DEnvname=SITENV clean test
-        
-        //Envname will be picked up either from -DEnvname from system variable or from Envname tag of POM.xml
-        System.out.println(System.getProperty("Envname"));		
-        
-        //UserName will be picked up either from -DUserName from system variable or from UserName tag of POM.xml
-        System.out.println(System.getProperty("UserName"));
+		//Envname will be picked up either from -DEnvname from system variable or from Envname tag of POM.xml
+		System.out.println(System.getProperty("Envname"));
 
-    }
+		//UserName will be picked up either from -DUserName from system variable or from UserName tag of POM.xml
+		System.out.println(System.getProperty("UserName"));
+		
+		WebDriverManager.chromedriver().setup();
+
+	}
 }
