361fc3e973c4f77154d2490eb45cbe4e3c0d28d8
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-01-26 11:22:55 +0530

    Updating driver container

--- src/test/java/MavenTutorial/MavenTutorial/AppTest.java
+++ src/test/java/MavenTutorial/MavenTutorial/AppTest.java
@@ -1,18 +1,20 @@
 package MavenTutorial.MavenTutorial;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-	@Test
-	public void testApp() throws InterruptedException
+	@Test(description = "Mavn profile and parameters")
+	public void mavenBasic() throws InterruptedException
 	{
 		//Without profile
 		//-DEnvname=UAT clean install
@@ -25,11 +27,18 @@ public class AppTest
 
 		//UserName will be picked up either from -DUserName from system variable or from UserName tag of POM.xml
 		System.out.println(System.getProperty("UserName"));
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\selenium_drivers\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	}
+	
+	@Test(description = "This part is used for docker")
+	public void testDocker() throws MalformedURLException, InterruptedException
+	{
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		URL url = new URL("http://192.168.99.100:4444/wd/hub/");
+		RemoteWebDriver driver = new RemoteWebDriver(url, cap);
 		driver.get("https://www.google.com");
 		Thread.sleep(3000);
+		String title = driver.getTitle();
+		System.out.println("Title is : "+title);
 		driver.quit();
 	}
 }
