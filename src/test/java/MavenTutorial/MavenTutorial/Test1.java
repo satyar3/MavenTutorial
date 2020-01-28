d0699b4b279f4609fabe7451d9512c26b4438837
Satyaranjan Muduli <satyaranjan.muduli@amadeus.com>
2020-01-28 21:50:31 +0530

    Selenium Grid Integration

---
+++ src/test/java/MavenTutorial/MavenTutorial/Test1.java
@@ -0,0 +1,22 @@
+package MavenTutorial.MavenTutorial;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+	@Test(description = "Mavn profile and parameters", priority = 0)
+	public void mavenBasic() throws InterruptedException
+	{
+		//Without profile
+		//-DEnvname=UAT clean install
+
+		//With profile
+		//-PSIT -DEnvname=SITENV clean test
+
+		//Envname will be picked up either from -DEnvname from system variable or from Envname tag of POM.xml
+		System.out.println(System.getProperty("Envname"));
+
+		//UserName will be picked up either from -DUserName from system variable or from UserName tag of POM.xml
+		System.out.println(System.getProperty("UserName"));
+	}
+}
---
+++ src/test/java/MavenTutorial/MavenTutorial/Test2.java
@@ -0,0 +1,24 @@
+package MavenTutorial.MavenTutorial;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Test2
+{
+	@Test(description = "This test is used for docker run", priority = 1)
+	public void testDocker() throws MalformedURLException, InterruptedException
+	{
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		URL url = new URL("http://192.168.99.100:4444/wd/hub/");
+		RemoteWebDriver driver = new RemoteWebDriver(url, cap);
+		driver.get("https://www.google.com");
+		Thread.sleep(3000);
+		String title = driver.getTitle();
+		System.out.println("Title is : "+title);
+		driver.quit();
+	}
+}
